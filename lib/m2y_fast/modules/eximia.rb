501d721f89918412fde8d46d0e20f8dadaf3e112
Danilo Sobral <danilo.sobral@mobile2you.com.br>
2021-11-23 13:21:18 -0300

    feat(ZEMA-1848): (improved PDF id response and eximia errors)

--- lib/m2y_fast/modules/eximia.rb
+++ lib/m2y_fast/modules/eximia.rb
@@ -5,6 +5,13 @@ module M2yFast
       xml = EximiaXmlBuilder.get_pdf_id_body(fast_account, due_date)
       response = M2yFast.configuration.eximia_client.call(:find_conta_dta_venc, xml: xml)
       full_response(EximiaXmlResponseParser.get_pdf_id_response(response.body), xml, response.body)
+    rescue StandardError => error
+      eximia_error(xml, error)
+    end
+
+    def self.eximia_error(request, error)
+      error_message = { error: true, message: error.message, code: error&.http&.code }
+      full_response(error_message, request, nil)
     end
   end
 end
